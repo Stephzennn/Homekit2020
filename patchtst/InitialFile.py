f3bc779913dc759fb0a37e0d3d05dda0f869992b
Estifanos Zenebe Gebresellassie <ezg6@gatech.edu>
2026-03-01 18:20:04 -0500

    Set up initial data access

---
+++ patchtst/InitialFile.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+ 
+import os
+
+print("Current working directory:", Path.cwd())
+
+
+#print(Path.home().parents[2]/ "scratch" / "ezg6" / "homekit2020")
+DATA_PATH = Path.home()/"scratch"/ "homekit2020-1.0" # / "homekit2020"  #.parents[2]#
+
+
+os.chdir(DATA_PATH)
+print("Current working directory:", Path.cwd())
+
+daily_survey_onehot_data_path = DATA_PATH / "daily_surveys_onehot.csv"
+
+print(daily_survey_onehot_data_path)
+
+import pandas as pd
+
+datasurvey = pd.read_csv(daily_survey_onehot_data_path)
+
+
+datasurvey.head()
+
+
+for item in Path.cwd().iterdir():
+    size = item.stat().st_size
+    print(f"{item.name}  |  {size} bytes")
